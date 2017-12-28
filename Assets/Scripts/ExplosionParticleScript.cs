a431b22a22b4d0ff26b3c9095d6000fff1518447
Ludovic <ludovic.mantov@gmail.com>
2017-12-28 23:58:46 +0100

    Ajout système explosion

---
+++ Assets/Scripts/ExplosionParticleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionParticleScript : MonoBehaviour {
+
+	public float explosionForce = 300f;
+	public float lifetime = 1f;
+
+	void Start () {
+		Vector3 randomDirection = new Vector3 (
+			Random.Range(-1f,1f),
+			Random.Range(-1f,1f),
+			Random.Range(-1f,1f)
+		);
+
+		float randomForce = Random.Range (0f, explosionForce);
+
+		GetComponent<Rigidbody> ().AddForce (randomDirection.normalized * randomForce);
+	}
+
+	void Update () {
+		lifetime -= Time.deltaTime;
+		if (lifetime <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/ExplosionScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour {
+
+	public GameObject[] explosionParticlePrefabs;
+	public int amountOfParticles = 3;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < amountOfParticles; i++) {
+			GameObject explosionParticlePrefab = Instantiate (explosionParticlePrefabs [Random.Range (0, explosionParticlePrefabs.Length)]);
+			explosionParticlePrefab.transform.position = transform.position;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
