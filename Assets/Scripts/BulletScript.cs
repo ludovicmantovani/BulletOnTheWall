5bb3a1d4dc1ff6b21cb8ffe5ad6e3145068fec73
Ludovic <ludovic.mantov@gmail.com>
2017-12-28 19:50:56 +0100

    Ajout force bullet + prefab

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    public float shootingForce = 1500f;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * shootingForce);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
